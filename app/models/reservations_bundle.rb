6a9a7d5a453bafa59cb25624911efa068f1f2395
Matúš Kmiť <matus.kmit@zhdk.ch>
2016-12-19 13:46:00 +0100

    fix reservations bundle query

--- app/models/reservations_bundle.rb
+++ app/models/reservations_bundle.rb
@@ -29,6 +29,7 @@ class ReservationsBundle < ActiveRecord::Base
       reservations.user_id,
       reservations.inventory_pool_id,
       reservations.delegated_user_id,
+      reservations.created_at,
       bool_or(groups.is_verification_required) AS verifiable_user,
       COUNT(partitions.id) > 0 AS verifiable_user_and_model,
       MAX(reservations.created_at) AS created_at
