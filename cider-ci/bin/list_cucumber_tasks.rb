a70b61f4108fe36f955d58eccb3e7bebeae02754
Max Albrecht <max.albrecht@zhdk.ch>
2016-12-21 23:30:52 +0100

    sort?

--- cider-ci/bin/list_cucumber_tasks.rb
+++ cider-ci/bin/list_cucumber_tasks.rb
@@ -38,7 +38,7 @@ leihs_feature_files = \
   Dir.glob('features/**/*.feature.disabled') -
   Dir.glob('engines/**/features/*')
 filepath = 'cider-ci/tasks/all_features.yml'
-create_feature_tasks(filepath, leihs_feature_files)
+create_feature_tasks(filepath, leihs_feature_files.sort)
 
 ENGINES.each do |engine|
   engine_feature_files = Dir.glob("engines/#{engine}/features/**/*.feature")
