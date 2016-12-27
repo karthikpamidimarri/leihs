2186b36b89ac4c2c3c53b07f4c3cb1c557cc5c55
Matúš Kmiť <matus.kmit@zhdk.ch>
2016-12-27 17:08:59 +0100

    fix partitions controller

--- app/controllers/manage/partitions_controller.rb
+++ app/controllers/manage/partitions_controller.rb
@@ -2,8 +2,7 @@ class Manage::PartitionsController < Manage::ApplicationController
 
   def index
     @partitions = \
-      Partition.with_generals(model_ids: params[:model_ids] \
-                              .map(&:presence).compact,
+      Partition.with_generals(model_ids: params[:model_ids],
                               inventory_pool_id: current_inventory_pool.id)
   end
 
