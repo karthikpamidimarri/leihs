694d288aa7d9e9f3c8f8d410e9d214bacd55fa13
Thomas Schank <DrTom@schank.ch>
2017-01-04 12:25:41 +0100

    Add create_edge for ModelGroupLink

--- app/models/model_group_link.rb
+++ app/models/model_group_link.rb
@@ -11,4 +11,8 @@ class ModelGroupLink < ActiveRecord::Base
     || where(child_id: mg1.id, parent_id: mg2.id).first
   end
 
+  def self.create_edge parent, child
+    create!(parent_id: parent.id, child_id: child.id)
+  end
+
 end
